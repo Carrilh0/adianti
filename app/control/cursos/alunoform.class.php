3652154357ace88af35a857c7e7a9e9bc0bb5da3
Vitor Carrilho <vitor.carrilho@hotmail.com>
2019-05-04 23:18:21 -0300

    Forms adcionados

---
+++ app/control/cursos/alunoform.class.php
@@ -0,0 +1,22 @@
+<?php
+
+class alunoform extends TPage{
+
+    private $form;
+
+    public function __construct(){
+        parent::__construct();
+
+        $this->form = new BootstrapFormBuilder();
+
+        $nome = new TEntry('nome');
+        $cursos = new TDBCombo('cursos_id','sample','Cursos','id','nome');
+
+        $this->form->addFields([new TLabel('Nome: ')], [$nome]);
+        $this->form->addFields([new TLabel('Cursos: ')], [$cursos]);
+
+        parent::add($this->form);
+
+    }
+
+}
